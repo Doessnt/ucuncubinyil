528c221ca140f915f7be24128bf774de4e49d974
Does <hpcodecraftt@gmail.com>
2022-12-22 19:41:20 +0300

    Liste Alıştırmalar

---
+++ liste2.py
@@ -0,0 +1,38 @@
+import random
+"""
+#Kodları anlamak için pythontutor.com
+liste = []
+for i in range(5):
+    user = input("Kelime alayım\n>>>> ")
+    liste.append(user)
+user2 = input("Aradığınız kelime\n>>>> ")
+
+if user2 in liste:
+    print("Buldum {}".format(user2))
+else:
+    print("Bulamadım")
+"""
+liste = []
+
+breakk = -1
+
+while True:
+    user = int(input("Sayı alayım\n>>>>"))
+    if user == breakk:
+        break
+    else:
+        liste.append(user)
+
+list2 =  []
+for i in liste:
+    if i % 2 == 0:
+        list2.append(i)
+
+print(list2)        
+
+
+
+#liste.sort()
+#print(liste)
+#liste.reverse()
+#print(liste)
