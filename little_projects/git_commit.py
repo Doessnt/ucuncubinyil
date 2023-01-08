a44e31b6f312844936c5774c694869412cb9db3d
JermarDev <hpcodecraftt@gmail.com>
2023-01-08 22:26:43 +0300

    Sahibim buraya bir şey yazmaya üşendi

---
+++ little_projects/git_commit.py
@@ -0,0 +1,19 @@
+import os 
+
+
+def git_commit(folder,commit_word = "Sahibim buraya bir şey yazmaya üşendi"):
+    folder = input("Lütfen dosya dizinini giriniz\n >>>> ")
+    os.chdir(folder)
+    os.getcwd()
+    os.system("git add .")
+    os.system(f'git commit -m "{commit_word}"')
+
+
+folder = input("Lütfen dosya dizinini giriniz\n >>>> ")
+
+git_commit(folder)
+    
+
+
+
+
