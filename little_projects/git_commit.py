de524c07afaefa3107b2cb5a270ce54c98fecb3f
JermarDev <hpcodecraftt@gmail.com>
2023-01-08 22:35:22 +0300

    Sahibim buraya bir şey yazmaya üşendi

--- little_projects/git_commit.py
+++ little_projects/git_commit.py
@@ -1,13 +1,12 @@
 import os 
 
 
-def git_commit(folder,commit_word = "Sahibim buraya bir şey yazmaya üşendi"):
-    folder = input("Lütfen dosya dizinini giriniz\n >>>> ")
-    os.chdir(folder)
-    os.getcwd()
-    os.system("git add .")
-    os.system(f'git commit -m "{commit_word}"')
-    os.system("git push")
+def git_commit(folder = "",commit_word = "Sahibim buraya bir şey yazmaya üşendi"): #Burda ön tanımlı olarak gitin hata vermemesi için varsayılan bir bilgilendirme metni bıraktık.
+    os.chdir(folder) # Burda dosya konumuna gittik .
+    os.getcwd()# ufak kontrol .
+    os.system("git add .") #Repoya dosyaları kayıt ettik.
+    os.system(f'git commit -m "{commit_word}"') # repoya yazılıcak olan bilgilendirme metnini yazdık.
+    os.system("git push") # en son repoya gönderdik. 
 
 
 folder = input("Lütfen dosya dizinini giriniz\n >>>> ")
