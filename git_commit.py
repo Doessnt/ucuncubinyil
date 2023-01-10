130424f190c7a3bad2b476ba61fdb71d916dda4e
hpcodecraftt <hpcodecraftt@gmail.com>
2023-01-10 18:30:27 +0300

    git_commit.py test commit

--- git_commit.py
+++ git_commit.py
@@ -5,21 +5,22 @@ def git_commit(folder = "",commit_word = "Sahibim buraya bir şey yazmaya üşen
     os.chdir(folder) # Burda dosya konumuna gittik .
     os.getcwd()# ufak kontrol .
     os.system("git add .") #Repoya dosyaları kayıt ettik.
-    time.sleep(1)
+    time.sleep(2)
     print("Dosyalar repoya eklendi\n") #Bilgilendirmeler 
-    time.sleep(1)
+    time.sleep(2)
     os.system(f'git commit -m "{commit_word}"') # repoya yazılıcak olan bilgilendirme metnini yazdık.
     print("\n\n")
     print(f"Commit edildi. Bilgilendirme metni: '{commit_word}'") #Bilgilendirmeler 
-    time.sleep(1)
+    time.sleep(2)
     os.system("git push") # en son repoya gönderdik.
-    time.sleep(1)
+    time.sleep(2)
     print("Repoya gönderildi.") 
 
 
 folder = input("Lütfen dosya dizinini giriniz\n >>>> ")
+commit_word = input("Lütfen Bilgilendirme metnini yazınız\n >>>>")
 
-git_commit(folder)
+git_commit(folder, commit_word)
     
 
 
